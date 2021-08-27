59baab69d94e98f726f54c9324b36e98611d3f1c
Ding Harley Catubag <dingharley123@gmail.com>
2021-08-27 14:11:13 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+//@flow
+
+/**
+ * Clear all console first before using.
+ */
+console.clear();
+
+/**
+ * Require/Import every packages or components we need
+ * for our project.
+ */
+const express = require('express');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Client = require('./src/structure/Client.js');
+const Command = require('./src/structure/Command.js');
+
+//end import.
+
+/**
+ * Put packages initialization here. 
+ */
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+ 
+const client = new Client();
+
+fs.readdirSync('./src/commands')
+    .filter(file => file.endsWith('.js'))
+    .forEach(file => {
+        /**
+         * @type {Command}
+         */
+        const command = require(`./src/commands/${file}`);
+        client.commands.set(command.name, command)
+    })
+
+//end initialization.
+
+client.on('messageCreate', message => {
+    //if prefix is not use lets return noting and end the function.
+    if(!message.content.startsWith(process.env.prefix)) return;
+    const splittedMessage = message.content.split(/ +/);
+    const command = client.commands.find(command => command.name == splittedMessage[0].slice(1, splittedMessage[0].length));
+
+    if(!command) return message.reply('There is no command like that you piece of shit.')
+    
+    command.run(message, splittedMessage, client);
+})
+/**
+ * This is responsible for our bot login. 
+ */
+client.login(process.env.DISCORD_KEY)
+
+app.listen(port, () => console.log(`App is running in port ${port}`));
---
+++ src/commands/handleName.js
@@ -0,0 +1,26 @@
+const Command = require('../structure/Command.js');
+
+module.exports = new Command({
+    name: 'harvesttime',
+    description: 'Returns harvest time.',
+    async run(message, args, client) {
+        const hour = args[1];
+        const minute = args[2];
+        const dates = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+        if(!hour || !minute) {
+            return message.reply('Invalid command, the command need to have hour and minute. -harvesttime (hour) (minute) ex. harvesttime 34 23')
+        }
+        
+        const thisTime = new Date();
+        const thisHour = thisTime.getTime();
+        const timeTobeAdded = (hour * 60 * 60 * 1000) + (minute * 60 * 1000);
+        const timeTobeReply = new Date(thisHour + timeTobeAdded);
+
+        const hourTobeReply = timeTobeReply.getHours() < 10 ? `0${timeTobeReply.getHours()}` : timeTobeReply.getHours() > 12 ? timeTobeReply.getHours() - 12 : timeTobeReply.getHours();
+        const minuteTobeReply = timeTobeReply.getMinutes() < 10 ? `0${timeTobeReply.getMinutes()}` : timeTobeReply.getMinutes();
+        const AMPM = timeTobeReply.getHours() < 12 ? 'AM' : 'PM';
+        
+        message.reply(`${dates[timeTobeReply.getDay() - 1]}, ${timeTobeReply.getDate()}/${timeTobeReply.getMonth()}/${timeTobeReply.getFullYear()} ${hourTobeReply}:${minuteTobeReply} ${AMPM}`)
+    }
+})
