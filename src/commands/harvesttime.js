9796dafdcd0a8f4eaaf6273acf5a4e8ce9e2f81a
Ding Harley Catubag <dingharley123@gmail.com>
2021-09-03 15:55:16 +0800

    #commit ht changes

--- src/commands/harvesttime.js
+++ src/commands/harvesttime.js
@@ -12,14 +12,25 @@ const letterChecker = (time) => {
 
 module.exports = new Command({
     name: 'ht',
-    description: 'Returns harvest from the given hour/s and minute/s. Example: -ht 30 30',
+    description: 'Returns harvest from the given hour/s and minute/s. Example: -ht 30 30 or -ht 30:30',
     async run(message, args, client) {
-        const hour = args[1];
-        const minute = args[2];
-        const dates = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        let hour;
+        let minute;
+        let dates = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        if(!hour || !minute || args.length > 3) {
-            return message.reply('Invalid command, the command need to have hour and minute. -harvesttime (hour) (minute) ex. harvesttime 34 23')
+        if(args.length === 2) {
+            const splittedData = args[1].split(":");
+            if(splittedData.length === 2) {
+                hour = splittedData[0];
+                minute = splittedData[1];
+            }
+        } else {
+            hour = args[1];
+            minute = args[2];
+        }
+
+        if(args.length > 3) {
+            return message.reply('Invalid command, the command need to have hour and minute. -harvesttime (hour) (minute) ex. -ht 34 23 or -ht 34:23')
         }
 
         if(letterChecker(hour) || letterChecker(minute)) {
@@ -31,10 +42,14 @@ module.exports = new Command({
         const timeTobeAdded = (hour * 60 * 60 * 1000) + (minute * 60 * 1000);
         const timeTobeReply = new Date(thisHour + timeTobeAdded);
 
+        if(!timeTobeAdded && timeTobeAdded !== 0) {
+            return message.reply('Invalid command, the command need to have hour and minute. -harvesttime (hour) (minute) ex. -ht 34 23 or -ht 34:23')
+        }
+
         const hourTobeReply = timeTobeReply.getHours() < 10 ? `0${timeTobeReply.getHours()}` : timeTobeReply.getHours() > 12 ? timeTobeReply.getHours() - 12 : timeTobeReply.getHours();
         const minuteTobeReply = timeTobeReply.getMinutes() < 10 ? `0${timeTobeReply.getMinutes()}` : timeTobeReply.getMinutes();
         const AMPM = timeTobeReply.getHours() < 12 ? 'AM' : 'PM';
         
-        message.reply(`${dates[timeTobeReply.getDay() === 0 ? dates.length - 1 : timeTobeReply.getDay() - 1]}, ${timeTobeReply.getDate() < 10 ? `0${timeTobeReply.getDate()}` : timeTobeReply.getDate()}/${timeTobeReply.getMonth() < 10 ? `0${timeTobeReply.getMonth() + 1}` : timeTobeReply.getMonth() + 1}/${timeTobeReply.getFullYear()} ${hourTobeReply}:${minuteTobeReply} ${AMPM}`)
+        message.reply(`${dates[timeTobeReply.getDay()]}, ${timeTobeReply.getDate() < 10 ? `0${timeTobeReply.getDate()}` : timeTobeReply.getDate()}/${timeTobeReply.getMonth() < 10 ? `0${timeTobeReply.getMonth() + 1}` : timeTobeReply.getMonth() + 1}/${timeTobeReply.getFullYear()} ${hourTobeReply}:${minuteTobeReply} ${AMPM}`)
     }       
 })
